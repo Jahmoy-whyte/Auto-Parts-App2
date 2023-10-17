33eb478052ac04cd27fb5cb76e2965d8f9ecebb8
johnwata <divebackup152@gmail.com>
2023-10-17 14:27:16 -0600

    changed base url

--- src/services/helper/baseUrl.js
+++ src/services/helper/baseUrl.js
@@ -1,3 +1,3 @@
-//export const BASE_URL = "https://auto-parts-server-2lng.onrender.com";
+export const BASE_URL = "https://auto-parts-server-2lng.onrender.com";
 
-export const BASE_URL = "http://192.168.100.28:3000";
+//export const BASE_URL = "http://192.168.100.28:3000";
